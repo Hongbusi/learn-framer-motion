cf6b63b54f96029214c811c194fb658aea944f1b
Hongbusi <coderhbs@gmail.com>
2023-07-10 10:46:01 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion'
+
+function App() {
+  
+
+  return (
+    <>
+     <motion.div className='w-10 h-10 bg-red-50' animate={{ x: 100 }} />
+    </>
+  )
+}
+
+export default App
